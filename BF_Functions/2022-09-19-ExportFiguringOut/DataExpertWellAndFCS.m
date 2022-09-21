8dea8c1d473ca396bb319be7d5f4022161dffe98
BrockFletcher <88505907+BrockFletcher@users.noreply.github.com>
2022-09-21 18:34:11 -0500

    CloseToFunctionLevel

--- BF_Functions/2022-09-19-ExportFiguringOut/DataExpertWellAndFCS.m
+++ BF_Functions/2022-09-19-ExportFiguringOut/DataExpertWellAndFCS.m
@@ -1,45 +1,70 @@
-%User Inputs
+%% User Inputs
 CellCountPlane=1;
+OutputPerWell = false;
+OutputFCS = true;
+exportdir=char('D:\Dropbox (VU Basic Sciences)\Duvall Confocal\Duvall Lab\Jackey\2022-09-15-Gal8-ConjugatesChloroquine\Analysis\Outputs\FCSExport');
+
+%% Code 
 
 TestTable=Tablebig;
-TestTable2=TestTable(1:1068,:);
+TestTable2=TestTable;
 InputTable=TestTable2;
+
+
 PreppedTable=removevars(InputTable,["Centroid","BoundingBox"]);
 SumInt=(PreppedTable{:,"Area"}.*PreppedTable{:,"MeanIntensity"});
 PreppedTable.SumIntensity=SumInt;
- PreppedTable(ismember(PreppedTable.Cell,0),:)=[];
+PreppedTable(ismember(PreppedTable.Cell,0),:)=[];
 
-%% FlowCytometryDataPrep
-PerCellData=groupsummary(PreppedTable,["WellNum","TimeNum","Cell","AnaPass","ImgPlane"],{"all"},["Area","EquivDiameter","Extent","MeanIntensity","SumIntensity"]);
-FlowPrep=unstack(PerCellData,[7:width(PerCellData)],{'ImgPlane'});
-% FlowPrep2=unstack(FlowPrep,[6:width(FlowPrep)],{'AnaPass'});
-% FlowPrepFlip=rows2vars(FlowPrep);
-% FlowPrepFlip.Properties.RowNames=FlowPrepFlip.(1);
-% FlowPrepFlip=removevars(FlowPrepFlip,"OriginalVariableNames");
-% FlowReadyDataunique=unique(FlowPrepFlip,'stable');
-% PerCellClean=rows2vars(FlowPrep);
-% PerCellClean=removevars(PerCellClean,"OriginalVariableNames");
-FlowPrep(ismember(FlowPrep.Cell,0),:)=[];
-% PerCellClean(PerCellClean.c4>PerCellClean.c1,:)=[];
-FlowbyCell=unstack(FlowPrep,[6:width(FlowPrep)],{'AnaPass'});
-FlowbyCell2=convertvars(FlowbyCell,@isnumeric,'single');
-FlowbyCell2(~ismember(FlowbyCell2{:,CellCountPlane+3},1),:)=[];
-% FlowMax=max(FlowbyCell2)
-% ColText=cell2struct([],FlowbyCell2.Properties.VariableNames,1)
-FCSOutData=single(table2array(FlowbyCell2));
-% FlowGroupNames=get(FlowbyCell2,'columnname');
-   
-test2=cell2struct(Test,'PnN',1);
-% test2(1).BYTEORD = "1,2,3,4";
-TEXT.PnN = FlowbyCell2.Properties.VariableNames;
-PleaseWork=groupsummary(FlowbyCell2,'WellNum','max');
-FlowGroups=table2struct(PleaseWork);
-% writeFCS('test1.fcs',FCSOutData,FlowGroups);
-writeFCS('test1.fcs',FCSOutData,TEXT);
 
 
 %% WholeWellDataPrep
+if OutputPerWell
+
 WellTimeAnaPlaneData=groupsummary(PreppedTable,["WellNum","TimeNum","AnaPass","ImgPlane"],{"all"},["Area","EquivDiameter","Extent","MeanIntensity","SumIntensity"]);
 WellTimeAnaData=unstack(WellTimeAnaPlaneData,[5:width(WellTimeAnaPlaneData)],{'ImgPlane'});
 WellTimeData=unstack(WellTimeAnaData,[4:width(WellTimeAnaData)],{'AnaPass'});
+writetable(WellTimeData,strcat(exportdir,'WellTimeData.xlsx'));
+writetable(WellTimeAnaData,strcat(exportdir,'WellTimeAnaData.xlsx'));
+writetable(WellTimeAnaPlaneData,strcat(exportdir,'WellTimeAnaPlaneData.xlsx'));
+end
+
+%% FlowCytometryDataPrep
+
+if OutputFCS
+PerCellData = groupsummary(PreppedTable,["WellNum","TimeNum","Cell","AnaPass","ImgPlane"],{"all"},["Area","EquivDiameter","Extent","MeanIntensity","SumIntensity"]);
+FlowbyCellAnaPlane=removevars(PerCellData,["GroupCount"]);
+% FlowPrep(ismember(FlowPrep.Cell,0),:)=[];
+
+FlowbyCellPlane=unstack(FlowbyCellAnaPlane,[6:width(FlowPrep)],{'AnaPass'});
+FlowbyCell=unstack(FlowbyCellPlane,[5:width(FlowbyCellPlane)],{'ImgPlane'});
+
+FlowbyCell2=convertvars(FlowbyCell,@isnumeric,'single');
+% FlowbyCell2(~ismember(FlowbyCell2{:,CellCountPlane+3},1),:)=[];
+FCSOutData=single(table2array(FlowbyCell2));
+WellColumn=FCSOutData(:,1);
+TimeColumn=FCSOutData(:,2);
+TEXT.PnN = FlowbyCell2.Properties.VariableNames;
+PleaseWork=groupsummary(FlowbyCell2,'WellNum','max');
+
+%                FlowGroups=table2struct(PleaseWork);
+
+    for i=1:max(FlowbyCell2.WellNum)
+            
+            WellPoint = num2str(i,'%03.f');
+            CurrWell=FCSOutData(WellColumn==i,:);
+
+        for j=1:max(FlowbyCell2.TimeNum)
+                Timepoint = num2str(j,'%03.f'); %Creates a string so that the BioFormats can read it
+                CurrTime=CurrWell(TimeColumn==j,:);
+                
+                FCSFileName=strcat(exportdir,'\','w',WellPoint,'t',Timepoint,'.fcs') ; 
+     
+                writeFCS(FCSFileName,CurrTime,TEXT); 
+
+        end
+    end
+end
+
+
 
