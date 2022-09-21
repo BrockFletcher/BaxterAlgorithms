8d1242809588433dfbd0686db3ab82582d4d91ac
BrockFletcher <88505907+BrockFletcher@users.noreply.github.com>
2022-09-21 16:47:51 -0500

    WorkingExport

--- BF_Functions/2022-09-19-ExportFiguringOut/DataExpertWellAndFCS.m
+++ BF_Functions/2022-09-19-ExportFiguringOut/DataExpertWellAndFCS.m
@@ -1,7 +1,7 @@
 %User Inputs
-CellCountPlane=3;
+CellCountPlane=1;
 
-
+TestTable=Tablebig;
 TestTable2=TestTable(1:1068,:);
 InputTable=TestTable2;
 PreppedTable=removevars(InputTable,["Centroid","BoundingBox"]);
@@ -13,21 +13,29 @@ PreppedTable.SumIntensity=SumInt;
 PerCellData=groupsummary(PreppedTable,["WellNum","TimeNum","Cell","AnaPass","ImgPlane"],{"all"},["Area","EquivDiameter","Extent","MeanIntensity","SumIntensity"]);
 FlowPrep=unstack(PerCellData,[7:width(PerCellData)],{'ImgPlane'});
 % FlowPrep2=unstack(FlowPrep,[6:width(FlowPrep)],{'AnaPass'});
-FlowPrepFlip=rows2vars(FlowPrep);
-FlowPrepFlip.Properties.RowNames=FlowPrepFlip.(1);
-FlowPrepFlip=removevars(FlowPrepFlip,"OriginalVariableNames");
-FlowReadyDataunique=unique(FlowPrepFlip,'stable');
-PerCellClean=rows2vars(FlowReadyDataunique);
-PerCellClean=removevars(PerCellClean,"OriginalVariableNames");
-PerCellClean(ismember(PerCellClean.Cell,0),:)=[];
+% FlowPrepFlip=rows2vars(FlowPrep);
+% FlowPrepFlip.Properties.RowNames=FlowPrepFlip.(1);
+% FlowPrepFlip=removevars(FlowPrepFlip,"OriginalVariableNames");
+% FlowReadyDataunique=unique(FlowPrepFlip,'stable');
+% PerCellClean=rows2vars(FlowPrep);
+% PerCellClean=removevars(PerCellClean,"OriginalVariableNames");
+FlowPrep(ismember(FlowPrep.Cell,0),:)=[];
 % PerCellClean(PerCellClean.c4>PerCellClean.c1,:)=[];
-FlowbyCell=unstack(PerCellClean,[5:width(PerCellClean)],{'AnaPass'});
-FlowbyCell2=FlowbyCell;
+FlowbyCell=unstack(FlowPrep,[6:width(FlowPrep)],{'AnaPass'});
+FlowbyCell2=convertvars(FlowbyCell,@isnumeric,'single');
 FlowbyCell2(~ismember(FlowbyCell2{:,CellCountPlane+3},1),:)=[];
+% FlowMax=max(FlowbyCell2)
+% ColText=cell2struct([],FlowbyCell2.Properties.VariableNames,1)
 FCSOutData=single(table2array(FlowbyCell2));
 % FlowGroupNames=get(FlowbyCell2,'columnname');
-FlowGroups=table2struct(FlowbyCell2(1,:));
-writeFCS('test1.fcs',FCSOutData,FlowGroups);
+   
+test2=cell2struct(Test,'PnN',1);
+% test2(1).BYTEORD = "1,2,3,4";
+TEXT.PnN = FlowbyCell2.Properties.VariableNames;
+PleaseWork=groupsummary(FlowbyCell2,'WellNum','max');
+FlowGroups=table2struct(PleaseWork);
+% writeFCS('test1.fcs',FCSOutData,FlowGroups);
+writeFCS('test1.fcs',FCSOutData,TEXT);
 
 
 %% WholeWellDataPrep
