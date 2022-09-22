2a6e7409b55725129554cd8cb4b932ac98111ee5
BrockFletcher <88505907+BrockFletcher@users.noreply.github.com>
2022-09-21 23:14:54 -0500

    StillNeedFinalExportSettings
    Need to get Grouping Variable and a timepoint WELLID better incorporated for easier combination of the flow files in FlowJo. Also need to test FlowJo compatibility and determine why FloRead.io isn't working

--- BF_Functions/2022-09-19-ExportFiguringOut/DataExpertWellAndFCS.m
+++ BF_Functions/2022-09-19-ExportFiguringOut/DataExpertWellAndFCS.m
@@ -1,17 +1,18 @@
 %% User Inputs
 CellCountPlane=1;
-OutputPerWell = false;
+OutputPerWell = true;
 OutputFCS = true;
 exportdir=char('D:\Dropbox (VU Basic Sciences)\Duvall Confocal\Duvall Lab\Jackey\2022-09-15-Gal8-ConjugatesChloroquine\Analysis\Outputs\FCSExport');
 
 %% Code 
-
+% 
 TestTable=Tablebig;
 TestTable2=TestTable;
-InputTable=TestTable2;
+InputTable=TestTable2
 
 
 PreppedTable=removevars(InputTable,["Centroid","BoundingBox"]);
+PreppedTable=convertvars(PreppedTable,@isnumeric,'single');
 SumInt=(PreppedTable{:,"Area"}.*PreppedTable{:,"MeanIntensity"});
 PreppedTable.SumIntensity=SumInt;
 PreppedTable(ismember(PreppedTable.Cell,0),:)=[];
@@ -20,13 +21,13 @@ PreppedTable(ismember(PreppedTable.Cell,0),:)=[];
 
 %% WholeWellDataPrep
 if OutputPerWell
-
 WellTimeAnaPlaneData=groupsummary(PreppedTable,["WellNum","TimeNum","AnaPass","ImgPlane"],{"all"},["Area","EquivDiameter","Extent","MeanIntensity","SumIntensity"]);
 WellTimeAnaData=unstack(WellTimeAnaPlaneData,[5:width(WellTimeAnaPlaneData)],{'ImgPlane'});
 WellTimeData=unstack(WellTimeAnaData,[4:width(WellTimeAnaData)],{'AnaPass'});
-writetable(WellTimeData,strcat(exportdir,'WellTimeData.xlsx'));
-writetable(WellTimeAnaData,strcat(exportdir,'WellTimeAnaData.xlsx'));
-writetable(WellTimeAnaPlaneData,strcat(exportdir,'WellTimeAnaPlaneData.xlsx'));
+
+writetable(WellTimeData,strcat(exportdir,'\','WellTimeData.xlsx'));
+writetable(WellTimeAnaData,strcat(exportdir,'\','WellTimeAnaData.xlsx'));
+writetable(WellTimeAnaPlaneData,strcat(exportdir,'\','WellTimeAnaPlaneData.xlsx'));
 end
 
 %% FlowCytometryDataPrep
@@ -34,32 +35,32 @@ end
 if OutputFCS
 PerCellData = groupsummary(PreppedTable,["WellNum","TimeNum","Cell","AnaPass","ImgPlane"],{"all"},["Area","EquivDiameter","Extent","MeanIntensity","SumIntensity"]);
 FlowbyCellAnaPlane=removevars(PerCellData,["GroupCount"]);
-% FlowPrep(ismember(FlowPrep.Cell,0),:)=[];
 
-FlowbyCellPlane=unstack(FlowbyCellAnaPlane,[6:width(FlowPrep)],{'AnaPass'});
-FlowbyCell=unstack(FlowbyCellPlane,[5:width(FlowbyCellPlane)],{'ImgPlane'});
 
-FlowbyCell2=convertvars(FlowbyCell,@isnumeric,'single');
-% FlowbyCell2(~ismember(FlowbyCell2{:,CellCountPlane+3},1),:)=[];
-FCSOutData=single(table2array(FlowbyCell2));
-WellColumn=FCSOutData(:,1);
-TimeColumn=FCSOutData(:,2);
-TEXT.PnN = FlowbyCell2.Properties.VariableNames;
-PleaseWork=groupsummary(FlowbyCell2,'WellNum','max');
+FlowbyCellPlane=unstack(FlowbyCellAnaPlane,[6:width(FlowbyCellAnaPlane)],{'AnaPass'});
+ FlowbyCell=unstack(FlowbyCellPlane,[5:width(FlowbyCellPlane)],{'ImgPlane'});
 
-%                FlowGroups=table2struct(PleaseWork);
+ FCSOutData=single(table2array(FlowbyCell));
+FCSOutData(isnan(FCSOutData))=0;
+ 
+ WellColumn=FCSOutData(:,1);
+ TimeColumn=FCSOutData(:,2);
+TEXT.PnN = FlowbyCell.Properties.VariableNames;
 
-    for i=1:max(FlowbyCell2.WellNum)
+
+
+    for i=1:max(FlowbyCell.WellNum)
             
             WellPoint = num2str(i,'%03.f');
             CurrWell=FCSOutData(WellColumn==i,:);
-
-        for j=1:max(FlowbyCell2.TimeNum)
+            TimeColumn=CurrWell(:,2);
+        for j=1:max(TimeColumn)
                 Timepoint = num2str(j,'%03.f'); %Creates a string so that the BioFormats can read it
                 CurrTime=CurrWell(TimeColumn==j,:);
-                
-                FCSFileName=strcat(exportdir,'\','w',WellPoint,'t',Timepoint,'.fcs') ; 
-     
+                WellID=strcat('w',WellPoint,'t',Timepoint)
+                FCSFileName=strcat(exportdir,'\',WellID,'.fcs') ; 
+                TEXT.WELLID=WellID;
+                TEXT.FIL=WellID;
                 writeFCS(FCSFileName,CurrTime,TEXT); 
 
         end
